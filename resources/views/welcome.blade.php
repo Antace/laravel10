41425e6097e66217be7f4fb16427fb1fe740d0ad
antace <earthyon@hotmail.com>
2024-03-01 16:32:38 +0700

    Update welcome.blade.php

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -6,7 +6,5 @@
     <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Asperiores tempora voluptatibus reiciendis expedita
         harum, non doloremque ullam architecto veniam corrupti modi reprehenderit eaque pariatur velit alias autem
         soluta dolore. Magnam.</p>
-    <a href="/blog">Blog</a>
-    <br>
-    <a href="/about">About Us</a>
+    
 @endsection
