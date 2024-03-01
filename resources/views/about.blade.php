d5e4b9e2092f6490c2e7a20e9faa2a7eb0084aff
antace <earthyon@hotmail.com>
2024-03-01 16:32:28 +0700

    Update about.blade.php

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -2,10 +2,12 @@
 @section('title', 'about')
 @section('content')
     <h2>Aboout Us</h2>
+    <hr>
+    <p>Dev. : {{$name}}</p>
+    <p>Date : {{$date}}</p>
     <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Asperiores tempora voluptatibus reiciendis expedita harum,
         non doloremque ullam architecto veniam corrupti modi reprehenderit eaque pariatur velit alias autem soluta dolore.
         Magnam.</p>
-    <a href="/about">About Us</a>
-    <br>
-    <a href="/">Home</a>
+
+    
 @endsection
