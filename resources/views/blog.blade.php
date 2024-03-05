5725396442dbab1e02981994de5e50c1e0b7e3eb
antace <earthyon@hotmail.com>
2024-03-05 16:20:13 +0700

    Update blog.blade.php

--- resources/views/blog.blade.php
+++ resources/views/blog.blade.php
@@ -1,35 +1,47 @@
 @extends('layout')
 @section('title', 'blog')
 @section('content')
-    <h2 class="text-center py-2">Blogs</h2>
-    <a href="/create" class="btn btn-primary my-1">Add Content</a>
-    
-    <table class="table table-bordered text-center">
-        <thead>
-            <tr>
-                <th scope="col">Title</th>
-                <th scope="col">Content</th>
-                <th scope="col">Status</th>
-            </tr>
-        </thead>
-        <tbody>
-            @foreach ($blogs as $item)
+    @if (count($blogs) > 0){
+        <h2 class="text-center py-2">Blogs</h2>
+        <a href="/create" class="btn btn-primary my-1">Add Content</a>
+
+        <table class="table table-bordered text-center">
+            <thead>
                 <tr>
-                    <td>{{ $item['title'] }}</td>
-                    <td>{{ $item['content'] }}</td>
-                    <td>
-                    @if ($item['status'] == true)
-                        <a href="#" class="btn btn-success">เผยแพร่</a>
-                    @else
-                        <a href="#" class="btn btn-secondary">ฉบับร่าง</a>
-                    @endif
-                </td>
+                    <th scope="col">Title</th>
+                    {{-- <th scope="col">Content</th> --}}
+                    <th scope="col">Status</th>
+                    <th scope="col">Edit</th>
+                    <th scope="col">Delete</th>
                 </tr>
-            @endforeach
+            </thead>
+            <tbody>
+                @foreach ($blogs as $item)
+                    <tr>
+                        <td>{{ $item->title }}</td>
+                        {{-- <td>{{ Str::limit($item->content,10) }}</td> --}}
+                        <td>
+                            @if ($item->status == true)
+                                <a href="{{ route('change', $item->id) }}" class="btn btn-success">เผยแพร่</a>
+                            @else
+                                <a href="{{ route('change', $item->id) }}" class="btn btn-secondary">ฉบับร่าง</a>
+                            @endif
+                        </td>
+                        <td><a href="{{ route('edit', $item->id) }}" class="btn btn-warning">Edit</a></td>
+                        <td><a href="{{ route('delete', $item->id) }}" class="btn btn-danger"
+                                onclick="return confirm('คุณต้องการลบบทความหรือไม่ {{ $item->title }} ?')">Delete</a></td>
+                    </tr>
+                @endforeach
 
-        </tbody>
-    </table>
+            </tbody>
+        </table>
 
+        {{ $blogs->links() }}
+        }
+    @else{
+        <h2 class="text-center py-2">Empty</h2>
+    }
+    @endif
 
 
 
